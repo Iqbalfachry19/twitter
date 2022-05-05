7ca3b92a5190be7171e37f64edcfb381354219f4
iqbalfachry19 <ifachry@rocketmail.com>
2022-05-06 01:44:54 +0700

    hi

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -9,10 +9,12 @@ import {
   UserIcon,
   HomeIcon,
 } from '@heroicons/react/outline'
+import SidebarRow from './SidebarRow'
 function Sidebar() {
   return (
     <div>
       <img className="h-10 w-10" src="https://links.papareact.com/drq" alt="" />
+      <SidebarRow Icon={HomeIcon} title="Home" />
     </div>
   )
 }
--- components/SidebarRow.tsx
+++ components/SidebarRow.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
-
-function SidebarRow() {
-  return <div>SidebarRow</div>
+import React, { SVGProps } from 'react'
+interface Props {
+  Icon: (props: SVGProps<SVGSVGElement>) => JSX.Element
+  title: string
+}
+function SidebarRow({ Icon, title }: Props) {
+  return (
+    <div>
+      <Icon />
+      <p>{title}</p>
+    </div>
+  )
 }
 
 export default SidebarRow
