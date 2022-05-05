f80397d34a824f1b655227b6fb83c0fea4be4c25
iqbalfachry19 <ifachry@rocketmail.com>
2022-05-06 02:11:37 +0700

    hi

--- components/Widgets.tsx
+++ components/Widgets.tsx
@@ -1,7 +1,24 @@
+import { SearchIcon } from '@heroicons/react/outline'
 import React from 'react'
-
+import { TwitterTimelineEmbed } from 'react-twitter-embed'
 function Widgets() {
-  return <div>Widgets</div>
+  return (
+    <div className="mt-2 px-2">
+      <div className="mt-2 flex items-center space-x-2 rounded-full bg-gray-100 p-3">
+        <SearchIcon className="h-5 w-5 text-gray-400" />
+        <input
+          type="text"
+          placeholder="Search Twitter"
+          className="flex-1 bg-transparent outline-none"
+        />
+      </div>
+      <TwitterTimelineEmbed
+        sourceType="profile"
+        screenName="saurabhnemade"
+        options={{ height: 400 }}
+      />
+    </div>
+  )
 }
 
 export default Widgets
